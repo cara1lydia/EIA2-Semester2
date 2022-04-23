4f25759a2fe2bdb7e32774324bac628f139229d1
cara1lydia <92157012+cara1lydia@users.noreply.github.com>
2022-04-23 18:08:57 +0200

    L03

--- L03/L03_script.js
+++ L03/L03_script.js
@@ -3,13 +3,23 @@
     * Name: Cara Lydia Brüggendieck
     * Matrikel: 269899
     * Datum: 18.04.2022
+    * Quellen: Richard Mukasa, Ann-Kathrin Pfeffer
     */
 var Memory;
 (function (Memory) {
     window.addEventListener("load", handleLoad);
+    /** global variables */
     let fontColorGlobal;
-    let clickCounter = 0;
+    let countCards = 0;
     let values = [];
+    let timeStart;
+    let timeEnd;
+    let clickCounter = 1;
+    let card1;
+    let card2;
+    let cardHidden1;
+    let cardHidden2;
+    let move = false;
     function handleLoad() {
         document.querySelector("#btnStart").addEventListener("click", menu);
     }
@@ -19,27 +29,32 @@ var Memory;
         let formData = new FormData(document.forms[0]);
         for (let entry of formData.entries())
             console.log(entry);
+        /**variables for menu */
         let cardAmount = parseInt(formData.get("cardAmount").toString());
-        console.log(cardAmount);
         let cardSize = parseInt(formData.get("cardSize").toString());
-        console.log(cardSize);
         let backColor = formData.get("backColor").toString();
-        console.log(backColor);
         let cardColor = formData.get("cardColor").toString();
-        console.log(cardColor);
         let fontColor = formData.get("fontColor").toString();
-        console.log(fontColor);
         fontColorGlobal = fontColor;
         let fontStyle = formData.get("fontStyle").toString();
-        console.log(fontStyle);
+        /** remove menu form and start button when button clicked */
         wrapper.removeChild(document.querySelector("form"));
         wrapper.removeChild(document.querySelector("button"));
         wrapper.style.backgroundColor = backColor;
         document.body.style.color = fontColor;
         document.body.style.fontFamily = fontStyle;
-        iplmtValues(cardAmount);
-        iplmtCards(cardAmount, cardSize, cardColor);
+        createValues(cardAmount);
+        createCards(cardAmount, cardSize, cardColor);
     }
+    /** take put in amount and create as many values */
+    function createValues(_cardQuantity) {
+        for (let i = 1; i <= _cardQuantity; i++) {
+            values.push(i);
+            values.push(i);
+        }
+        shuffle(values);
+    }
+    /**shuffle */
     function shuffle(a) {
         var j, x, i;
         for (i = a.length - 1; i > 0; i--) {
@@ -50,15 +65,71 @@ var Memory;
         }
         return a;
     }
-    function iplmtValues(_cardAmount) {
-        /** */
-        for (let i = 1; i <= _cardAmount; i++) {
-            values.push(i);
-            values.push(i);
+    /**create put in card amount*2 in chosen size and color  */
+    function createCards(_cardAmount, _cardSize, _cardColor) {
+        let wrapper = document.getElementById("wrapper");
+        for (let x = 0; x < _cardAmount * 2; x++) {
+            let cardDiv = document.createElement("div");
+            countCards++;
+            console.log(countCards);
+            wrapper.appendChild(cardDiv);
+            cardDiv.classList.add("card");
+            cardDiv.style.width = _cardSize / 2 + "px";
+            cardDiv.style.height = _cardSize / 2 + "px";
+            cardDiv.style.backgroundColor = _cardColor;
+            cardDiv.style.color = _cardColor;
+            console.log(cardDiv);
+            /** values on cards */
+            cardDiv.innerHTML = `${values[x]}`;
+            console.log(values);
+            cardDiv.addEventListener("click", function () {
+                clickCard(cardDiv);
+            });
         }
-        shuffle(values);
+        timeStart = Date.now();
+        move = true;
     }
-    function iplmtCards(_cardAmount, _cardSize, _cardColor) {
+    function clickCard(_card) {
+        if (move == true) {
+            if (_card.style.color == _card.style.backgroundColor && clickCounter == 1) {
+                _card.style.color = fontColorGlobal;
+                card1 = _card;
+                clickCounter++;
+            }
+            else if (_card.style.color == _card.style.backgroundColor && clickCounter == 2) {
+                _card.style.color = fontColorGlobal;
+                card2 = _card;
+                move = false;
+                this.window.setTimeout(compareCards, 2000);
+            }
+            else if (_card.style.color == fontColorGlobal) {
+                return;
+            }
+        }
+    }
+    function compareCards() {
+        if (card1.innerText == card2.innerText) {
+            card1.classList.add("hidden");
+            cardHidden1 = card1;
+            countCards--;
+            card2.classList.add("hidden");
+            cardHidden2 = card2;
+            countCards--;
+            console.log(countCards);
+            if (countCards == 0) {
+                timeEnd = Date.now();
+                let timePast = timeEnd - timeStart;
+                let minutes = Math.floor(timePast / 60000);
+                let seconds = Math.floor(timePast % 60000 / 1000);
+                alert("You won! Your time: " + minutes + "min " + seconds + "sek");
+            }
+        }
+        else if (card1 != card2) {
+            card1.style.color = card1.style.backgroundColor;
+            card2.style.color = card2.style.backgroundColor;
+        }
+        move = true;
+        clickCounter = 1;
     }
 })(Memory || (Memory = {}));
 //# sourceMappingURL=L03_script.js.map
--- L03/L03_script.ts
+++ L03/L03_script.ts
@@ -3,58 +3,64 @@
     * Name: Cara Lydia Brüggendieck
     * Matrikel: 269899
     * Datum: 18.04.2022
+    * Quellen: Richard Mukasa, Ann-Kathrin Pfeffer
     */
 namespace Memory {
-    window.addEventListener ("load", handleLoad);
-
+    window.addEventListener("load", handleLoad);
+/** global variables */
     let fontColorGlobal: string;
-    let clickCounter: number = 0;
+    let countCards: number = 0;
     let values: number[] = [];
+    let timeStart: number;
+    let timeEnd: number;
+    let clickCounter: number = 1;
+    let card1: HTMLDivElement;
+    let card2: HTMLDivElement;
+    let cardHidden1: HTMLDivElement;
+    let cardHidden2: HTMLDivElement;
+    let move: boolean = false;
+
 
     function handleLoad(): void {
         document.querySelector("#btnStart").addEventListener("click", menu);
-
     }
 
     function menu(): void {
-    /**take put in numbers/colors/font from menu and adapt style of game*/
-    let wrapper: HTMLDivElement = <HTMLDivElement>document.getElementById("wrapper");
-    let formData: FormData = new FormData(document.forms[0]);
+        /**take put in numbers/colors/font from menu and adapt style of game*/
+        let wrapper: HTMLDivElement = <HTMLDivElement>document.getElementById("wrapper");
+        let formData: FormData = new FormData(document.forms[0]);
 
-    for (let entry of formData.entries())
-    console.log(entry);
+        for (let entry of formData.entries())
+        console.log(entry);
 
-    let cardAmount: number = parseInt(formData.get("cardAmount").toString());
-    console.log(cardAmount);
+        /**variables for menu */
+        let cardAmount: number = parseInt(formData.get("cardAmount").toString());
+        let cardSize: number = parseInt(formData.get("cardSize").toString());
+        let backColor: string = formData.get("backColor").toString();
+        let cardColor: string = formData.get("cardColor").toString();
+        let fontColor: string = formData.get("fontColor").toString();
+        fontColorGlobal = fontColor;
+        let fontStyle: string = formData.get("fontStyle").toString();
 
-    let cardSize: number = parseInt(formData.get("cardSize").toString());
-    console.log(cardSize);
+        /** remove menu form and start button when button clicked */
+        wrapper.removeChild(document.querySelector("form"));
+        wrapper.removeChild(document.querySelector("button"));
+        wrapper.style.backgroundColor = backColor;
+        document.body.style.color = fontColor;
+        document.body.style.fontFamily = fontStyle;
 
-    let backColor: string = formData.get("backColor").toString();
-    console.log(backColor);
-
-    let cardColor: string = formData.get("cardColor").toString();
-    console.log(cardColor);
-
-    let fontColor: string = formData.get("fontColor").toString();
-    console.log(fontColor);
-    fontColorGlobal = fontColor;
-
-    let fontStyle: string = formData.get("fontStyle").toString();
-    console.log(fontStyle);
-
-    wrapper.removeChild(document.querySelector("form"));
-    wrapper.removeChild(document.querySelector("button"));
-    wrapper.style.backgroundColor = backColor;
-    document.body.style.color = fontColor;
-    document.body.style.fontFamily = fontStyle;
-
-    iplmtValues(cardAmount);
-
-    iplmtCards(cardAmount, cardSize, cardColor); 
-
+        createValues(cardAmount);
+        createCards(cardAmount, cardSize, cardColor);
     }
-
+/** take put in amount and create as many values */
+    function createValues (_cardQuantity: number): void {
+        for (let i: number = 1; i <= _cardQuantity; i++) {
+            values.push(i);
+            values.push(i);
+        }
+        shuffle(values);
+    }
+/**shuffle */
     function shuffle(a: number[]): number[] {
         var j: number, x: number, i: number;
         for (i = a.length - 1; i > 0; i--) {
@@ -66,17 +72,88 @@ namespace Memory {
         return a;
     }
 
-    function iplmtValues (_cardAmount: number): void {
-        /** */
-        for (let i: number = 1; i <= _cardAmount; i++) {
-            values.push(i);
-            values.push(i);
+    /**create put in card amount*2 in chosen size and color  */
+    function createCards(_cardAmount: number, _cardSize: number, _cardColor: string): void {
+
+        let wrapper: HTMLDivElement = <HTMLDivElement>document.getElementById("wrapper");
+
+        for (let x: number = 0; x < _cardAmount * 2; x++) {
+            let cardDiv: HTMLDivElement = document.createElement("div");
+            countCards++;
+            console.log(countCards);
+
+            wrapper.appendChild(cardDiv);
+            cardDiv.classList.add("card");
+
+            cardDiv.style.width = _cardSize / 2 + "px";
+            cardDiv.style.height = _cardSize / 2 + "px";
+            cardDiv.style.backgroundColor = _cardColor;
+            cardDiv.style.color = _cardColor;
+            console.log(cardDiv);
+    /** values on cards */
+            cardDiv.innerHTML = `${values[x]}`;
+
+            console.log(values);
+
+            cardDiv.addEventListener("click", function (): void {
+                clickCard(cardDiv);
+            });
         }
-        shuffle(values);
+
+        timeStart = Date.now();
+        move = true;
     }
 
-    function iplmtCards(_cardAmount: number, _cardSize: number, _cardColor: string): void {
 
+
+    function clickCard(_card: HTMLDivElement): void {
+        if (move == true) {
+            if (_card.style.color == _card.style.backgroundColor && clickCounter == 1) {
+                _card.style.color = fontColorGlobal;
+                card1 = _card;
+                clickCounter++;
+            }
+            else if (_card.style.color == _card.style.backgroundColor && clickCounter == 2) {
+                _card.style.color = fontColorGlobal;
+                card2 = _card;
+
+                move = false;
+                this.window.setTimeout(compareCards, 2000);
+            }
+            else if (_card.style.color == fontColorGlobal) {
+                return;
+            }
+        }
     }
+
+
+    function compareCards(): void {
+        if (card1.innerText == card2.innerText) {
+            card1.classList.add("hidden");
+            cardHidden1 = card1;
+            countCards--;
+            card2.classList.add("hidden");
+            cardHidden2 = card2;
+            countCards--;
+            console.log(countCards);
+            if (countCards == 0) {
+                timeEnd = Date.now();
+                let timePast: number = timeEnd - timeStart;
+                let minutes: number = Math.floor(timePast / 60000);
+                let seconds: number = Math.floor(timePast % 60000 / 1000);
+                alert("You won! Your time: " + minutes + "min " + seconds + "sek");
+            }
+        }
+        else if (card1 != card2) {
+            card1.style.color = card1.style.backgroundColor;
+            card2.style.color = card2.style.backgroundColor;
+        }
+        move = true;
+        clickCounter = 1;
+    }
+
+
+
+
 
 }
